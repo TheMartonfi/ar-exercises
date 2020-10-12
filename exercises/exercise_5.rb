e34b65a664621d7e0b9a881d525e6cba34b925a5
TheMartonfi <carlmartonfi@hotmail.com>
2020-10-12 23:22:45 +0000

    Finish exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,7 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+p "Annual revenue #{Store.sum("annual_revenue")}"
+p "Average annual revenue #{Store.average("annual_revenue").to_i}"
+p Store.where("annual_revenue > ?", 1000000).count
