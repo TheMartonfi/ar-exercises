77e510a581448245871a8e91242879d4e11bbb9f
TheMartonfi <carlmartonfi@hotmail.com>
2020-10-12 22:31:36 +0000

    Finish exercise 2

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,33 +5,26 @@ puts "----------"
 
 # Your code goes below here ...
 
-storeOne = Store.new do |s|
+Store.new do |s|
   s.name = "Burnaby"
   s.annual_revenue = 300000
   s.mens_apparel = true
   s.womens_apparel = true
-end
+end.save
 
-storeOne.save
-
-storeTwo = Store.new do |s|
+Store.new do |s|
   s.name = "Richmond"
   s.annual_revenue = 1260000
   s.mens_apparel = false
   s.womens_apparel = true
-end
+end.save
 
-storeTwo.save
-
-storeThree = Store.new do |s|
+Store.new do |s|
   s.name = "Gastown"
   s.annual_revenue = 190000
   s.mens_apparel = true
   s.womens_apparel = false
-end
+end.save
 
-storeThree.save
-
-stores = Store.all
-
-p stores
+p Store.count
+  
