cdb9deebaa499f8e083a48c99bf0300eff4e5d4d
TheMartonfi <carlmartonfi@hotmail.com>
2020-10-12 23:59:52 +0000

    Finish exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,4 +10,8 @@ puts "----------"
 
 # Your code goes here ...
 
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Bob", last_name: "Burger", hourly_rate: 45)
 
+@store2.employees.create(first_name: "Timmy", last_name: "Taco", hourly_rate: 40)
+@store2.employees.create(first_name: "Mang0", last_name: "GOAT", hourly_rate: 100)
